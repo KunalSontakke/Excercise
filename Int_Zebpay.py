6ed2c0e911e7b2cd1644ca2c6aab911610ccec3c
kunalsontakke <kunalsontakke1994@gmail.com>
2023-11-30 23:49:33 +0530

    changes made

---
+++ Int_Zebpay.py
@@ -0,0 +1,39 @@
+# """
+# +12,100
+#
+# """
+# inp = "+12,100"
+# res = ""
+# for i in inp:
+#     if i.isdigit():
+#         res += i
+# print(res)
+#
+#
+# # =====================
+# """
+# inp = 12100
+# out = 120
+# """
+# inp1 = "12100"
+# out = ""
+# for i in inp1:
+#     if i not in out:
+#         out += i
+# print(out)
+#
+# """
+# 1
+# 12
+# 123
+# 1234
+# 12345
+#
+# """
+# for i in range(6):
+#     for j in range(1,i+1):
+#         print(j,end=" ")
+#     print()
+#
+
+
